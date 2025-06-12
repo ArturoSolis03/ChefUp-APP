b2692ac90bd897c33b518d2e99ced2469240c00f
ArturoSolis03 <232940@ust.com>
2025-06-12 14:44:24 -0600

    Change some logic in the favorite service

--- backend/favorites-service/src/guards/jwt.strategy.ts
+++ backend/favorites-service/src/guards/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.ACCESS_TOKEN_SECRET || 'default_secret', // <- valor por defecto
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET || 'default_secret',
     });
   }
 
