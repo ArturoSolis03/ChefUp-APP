56e0052a711e112dd3f9508b860494f876e0b29e
ArturoSolis03 <232940@ust.com>
2025-06-16 09:21:00 -0600

    change id by sub

--- backend/favorites-service/src/auth/jwt.strategy.ts
+++ backend/favorites-service/src/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: any) {
     return {
-      userId: payload.id,
+      sub: payload.id,
       email: payload.email,
       name: payload.name,
     };
