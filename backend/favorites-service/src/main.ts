f660475cbe60fcbb5a1d11f8638de71977d42eeb
ArturoSolis03 <232940@ust.com>
2025-06-18 09:39:02 -0600

    Add cors policy

--- backend/favorites-service/src/main.ts
+++ backend/favorites-service/src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
